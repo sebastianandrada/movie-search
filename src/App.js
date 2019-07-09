030d70894a453df87aaaffa91053b5c2e3306b95
sebastianandrada <andrada.sebastia93@gmail.com>
2019-07-09 00:32:54 -0300

    consumo datos del servidor y muestro en pantalla

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import { Title } from './components/Title'
 import { SearchForm} from './components/SearchForm'
 import './App.css';
 import 'bulma/css/bulma.css'
+import { MoviesList } from './components/MoviesList';
 
 class App extends Component {
+  state = { results: [] }
+
+  _handleResults = (results) => {
+    this.setState({ results })
+  }
+
   render() {
     return (
       <div className="App">
         <Title>Search Movies</Title> 
         <div className='SearchForm-wrapper'>
-          <SearchForm />
+          <SearchForm onResults={this._handleResults} />
         </div>
-        
+        {this.state.results.length === 0
+          ? <p>Sin resultados</p>
+          : <MoviesList movies={this.state.results} />
+        }
       </div>
     )
   }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react"
 
+const API_KEY = '6e82ec76'
 export class SearchForm extends Component {
   state = {
-    inputMovie: ""
+    inputMovie: ''
   }
 
   _handleChange = e => {
@@ -11,7 +12,15 @@ export class SearchForm extends Component {
 
   _handleSubmit = e => {
     e.preventDefault()
-    alert(this.state.inputMovie)
+    const { inputMovie } = this.state
+
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+      .then(res => res.json())
+      .then(results => {
+        const { Search, totalResults} = results
+        console.log({ Search, totalResults })
+        this.props.onResults(Search)
+      })
   }
 
   render() {
